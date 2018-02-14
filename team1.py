99aff452b5cfbda01ad4b01e6ebe409e9c7cd080
whitneyg <whitney.garrett@sccsd.k12.ar.us>
2018-02-14 10:13:04 -0600

    chanded are name and strats

--- team1.py
+++ team1.py
@@ -6,11 +6,12 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'superman' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Comeback Kids' # Only 10 chars displayed.
+strategy_name = 'The comeback'
+strategy_description = 'when they betray, we betray'
     
 def move(my_history, their_history, my_score, their_score):
+    
 
 
     
