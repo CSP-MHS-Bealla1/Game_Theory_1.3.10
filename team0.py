6a244e2456dc8cf436c0b3e2e318e1a01819eceb
RG07030 <36272650+RG07030@users.noreply.github.com>
2018-02-15 09:55:27 -0600

    just deleted a line
    boi

--- team0.py
+++ team0.py
@@ -15,7 +15,7 @@ def move(my_history, their_history, my_score, their_score):
         return 'b'
     else:
         return 'c'
-    return 'c'
+    
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
